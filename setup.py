feead496fbc8967fbaac48614527eb87e69bf857
Ankit <ankitsinha1952@gmail.com>
2025-07-15 14:22:51 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='greenvision_ml',  
+    version='0.1.0',
+    author='Ankit',
+    author_email='ankitsinha1952@gmail.com',
+    packages=find_packages(),
+    install_requires=[] )
